c6365bfb76ec60235949a8850e8d7e21ffcf1a56
Toqa-25 <toqa35abdelsalam757@gmail.com>
2022-02-06 05:22:07 +0200

    upload elzeroAcdemey-Templete  files

---
+++ .history/script_20220116151124.js
@@ -0,0 +1,5 @@
+let otherParent = document.getElementById("parent-other-links");
+let otherChild = document.getElementById("child-other-links");
+otherParent.onclick = function () {
+  otherChild.classList.to("other");
+};
