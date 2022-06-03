30f0da2f457e045ed6ca158cf82e65e852a072ac
Toqa-25 <toqa35abdelsalam757@gmail.com>
2022-06-03 03:25:57 +0200

    upload loading page

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ let otherChild = document.getElementById("child-other-links");
 otherParent.onclick = function () {
   otherChild.classList.toggle("other");
 };
+
+
+
+window.addEventListener("load" ,function (e){
+  document.querySelector(".load-page").style.display="none";
+  console.log("loaded");
+  document.querySelector("body").style.cssText="overflow-y: auto;";
+ 
+    } )
